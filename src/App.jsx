3afeb5049833556482dfdc405837dff923b41530
Pacio Pacio <vinicius0404@gmail.com>
2021-07-17 10:36:36 -0300

    atualizando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Header from "./components/Header";
 import PopularProducts from './components/PopularProducts';
+import Footer from './components/Footer'
 
 import Banner from './images/Banner-1.jpg'
 
@@ -11,10 +12,19 @@ function App() {
   return (
     <>
       <Header/>
-      <img src={Banner} id={style.Banner} alt="" />
-      <span>Mais vendidos</span>
-      <span id={style.line}>line</span>
-      <PopularProducts/>
+      <img src={Banner} id={style.Banner} alt="" />     
+      <span>mais vendidos</span>
+      <span>line</span>
+      <PopularProducts/>      
+      <form className={style.news} action="">
+        <span>Participe de nossas news com promoções e novidades!</span>
+        <div className={style.register}>          
+          <input className={style.input} type="text" placeholder="Digite de nome" />
+          <input className={style.input} type="text" placeholder="Digite de email"/>
+          <button type="submit">Eu quero!</button>
+        </div>
+      </form>
+      <Footer/>
     </>
   );
 }
--- src/service/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export const api=axios.create({
-    baseUrl:'https://corebiz-test.herokuapp.com/api/v1/products'
-})
